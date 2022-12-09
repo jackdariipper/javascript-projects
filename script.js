2cae0d49505dce57ae72919f9c98fa733e64b567
jackdariipper <sixxxxthihanaing@gmail.com>
2022-12-09 20:51:34 +0630

    updated

--- script.js
+++ script.js
@@ -1,59 +1,58 @@
-const quoteContainer = document.getElementById('quote-container');
-const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
+const quoteText = document.getElementById('quote');
 const twitterButton = document.getElementById('twitter');
 const newQuoteButton = document.getElementById('new-quote');
-const loader = document.getElementById('loader')
+const quoteContainer = document.getElementById('quote-container');
+const loader = document.getElementById('loader');
 
-let quotes = [];
+let quote = [];
 
-function loading(){
-    loader.hidden = false;
+function loading() {
     quoteContainer.hidden = true;
+    loader.hidden = false;
 }
 
-function complete(){
-    loader.hidden = true;
+function complete() {
     quoteContainer.hidden = false;
+    loader.hidden = true;
 }
 
-function getNewQuote(){
+async function getQuoteFromApi(){
     loading();
-    let quote = quotes[Math.floor(Math.random() * quotes.length)]
-    if(!quote.author){
+    const apiUrl = "https://type.fit/api/quotes";
+    try{
+        const quotes = await fetch(apiUrl);
+        quote = await quotes.json();
+        getQuote();
+    }catch (err){
+        alert("nothing found");
+    }
+}
+
+function getQuote(){
+    loading();
+    const randomQuote = quote[Math.floor(Math.random() * quote.length)];
+    if(!randomQuote.author){
         authorText.textContent = "Unknown Author";
-    }else{
-        authorText.textContent = quote.author;
+    }else {
+        authorText.textContent = randomQuote.author;
     }
 
-    if(quote.text.length > 120){
+    if (randomQuote.text.length > 120){
         quoteText.classList.add('long-quote');
     }else {
         quoteText.classList.remove('long-quote');
     }
-    quoteText.textContent = quote.text;
+    quoteText.textContent = randomQuote.text;
     complete();
 }
 
-async function getQuotesByAPI(){
-    loading();
-    const apiUrl = "https://type.fit/api/quotes";
-    try {
-        const quoteList = await fetch(apiUrl);
-        quotes = await quoteList.json();
-        getNewQuote();
-    }catch (err){
-        alert("Nothing found");
-    }
-}
-
 function tweetQuote(){
-    const twitterPreTweetUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
-    window.open(twitterPreTweetUrl, '_blank');
+    const tweetUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(tweetUrl, "_blank");
 }
 
-//Event Listeners
-twitterButton.addEventListener("click", tweetQuote);
-newQuoteButton.addEventListener("click", getNewQuote);
+twitterButton.addEventListener('click', tweetQuote);
+newQuoteButton.addEventListener('click', getQuote);
 
-getQuotesByAPI();
+getQuoteFromApi();
